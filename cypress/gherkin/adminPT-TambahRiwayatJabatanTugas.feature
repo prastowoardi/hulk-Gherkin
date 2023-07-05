fb48281d387a5cf22645772085a317c981e4eb6a
prastowoardi <prastowoardi616@gmail.com>
2023-07-05 13:09:59 +0700

    Update

--- cypress/gherkin/adminPT-TambahRiwayatJabatanTugas.feature
+++ cypress/gherkin/adminPT-TambahRiwayatJabatanTugas.feature
@@ -3,20 +3,12 @@ Feature: User Login
     # Background: 
     #     Given User login sebagai "Admin" dan menambahkan data jabatan tugas
 
-    Scenario: (+)Admin menambahkan data jabatan tugas
+    Scenario: Admin menambahkan data jabatan tugas
         Given User login sebagai "Admin"
-        When Admin klik tombol tambah 
-        * field "Jabatan Tugas" dengan data "lainnya"
-        * field "Kategori Kegiatan" dengan data "duduk"
+        When Admin mengisi form dengan benar
         Then User berhasil menambahkan data
 
-    # Scenario: (-)Admin mengosongkan field 'Jabatan Tugas'
-    #     Given User login sebagai "Admin"
-    #     When Admin mengisi form
-    #     * 
-    #     Then User berhasil menambahkan data
-
-    # Scenario: (+)Dosen menambahkan data jabatan tugas
-    #     Given User login sebagai "Dosen"
-    #     When Dosen mengisi form dengan benar
-    #     Then User berhasil menambahkan data
+    Scenario: Dosen menambahkan data jabatan tugas
+        Given User login sebagai "Dosen"
+        When Dosen mengisi form dengan benar
+        Then User berhasil menambahkan data
--- cypress/step_definition/adminPT-TambahRiwayatJabatanTugas.js
+++ cypress/step_definition/adminPT-TambahRiwayatJabatanTugas.js
@@ -16,20 +16,15 @@ Given("User login sebagai {string}", (userRole) => {
     
 })
 
-When("field {string} dengan data {string}", (fieldName, fieldValue) => {
+When("Admin mengisi form dengan benar", () => {
     cy.visit("hr/list_rjabatanstruktural/7")
-    cy.get(".btn.btn-success.btn-sm").contains('Tambah Baru').click()
-    
-    if(fieldName == "Jabatan Tugas"){
-       //Jabatan Tugas
-        cy.get('#select2-idjabatantugas-container').click()
-        cy.get('.select2-search__field').type(fieldValue+"{enter}")
-    } else if(fieldName == "Kategori Kegiatan"){
-        //Kategori Kegiatan
-        cy.get('#select2-idrubrikkegiatan-container').click()
-        cy.get('.select2-search__field').type(fieldValue+"{enter}")
-    }
-    
+    cy.get(".btn").contains('Tambah Baru').click()
+    //Jabatan Tugas
+    cy.get('#select2-idjabatantugas-container').click()
+    cy.get('.select2-search__field').type("lainnya{enter}")
+    //Kategori Kegiatan
+    cy.get('#select2-idrubrikkegiatan-container').click()
+    cy.get('.select2-search__field').type("duduk{enter}")
     // SK Kegiatan
     cy.get('#iddokumen_label').click()
     cy.get('.odd > .text-center > .btn > .fa').click()
@@ -43,49 +38,27 @@ When("field {string} dengan data {string}", (fieldName, fieldValue) => {
 
 })
 
-// When("Admin mengisi form dengan benar", () => {
-//     cy.visit("hr/list_rjabatanstruktural/7")
-//     cy.get(".btn.btn-success.btn-sm").contains('Tambah Baru').click()
-//     //Jabatan Tugas
-//     cy.get('#select2-idjabatantugas-container').click()
-//     cy.get('.select2-search__field').type("lainnya{enter}")
-//     //Kategori Kegiatan
-//     cy.get('#select2-idrubrikkegiatan-container').click()
-//     cy.get('.select2-search__field').type("duduk{enter}")
-//     // SK Kegiatan
-//     cy.get('#iddokumen_label').click()
-//     cy.get('.odd > .text-center > .btn > .fa').click()
+When("Dosen mengisi form dengan benar", () => {
+    cy.visit('hr/list_rjabatanstruktural')
+    cy.get(".btn").contains('Tambah Baru').click()
+    //Jabatan Tugas
+    cy.get('#select2-idjabatantugas-container').click()
+    cy.get('.select2-search__field').type("lainnya{enter}")
+    //Kategori Kegiatan
+    cy.get('#select2-idrubrikkegiatan-container').click()
+    cy.get('.select2-search__field').type("duduk{enter}")
+    // SK Kegiatan
+    cy.get('#iddokumen_label').click()
+    cy.get('.odd > .text-center > .btn > .fa').click()
 
-//     cy.get('#tglterhitungmulai').type("18-07-2023")
-//     cy.get('#lokasipenugasan').type("Jakarta Pusat")
+    cy.get('#tglterhitungmulai').type("30-08-2023")
+    cy.get('#lokasipenugasan').type("Muara Enim")
 
-//     cy.get('.btn-success').contains("Simpan").click()
+    cy.get('.btn-success').contains("Simpan").click()
 
-//     cy.get('.modal-footer > .btn-primary').click()
+    cy.get('.modal-footer > .btn-primary').click()
 
-// })
-
-// When("Dosen mengisi form dengan benar", () => {
-//     cy.visit('hr/list_rjabatanstruktural')
-//     cy.get(".btn").contains('Tambah Baru').click()
-//     //Jabatan Tugas
-//     cy.get('#select2-idjabatantugas-container').click()
-//     cy.get('.select2-search__field').type("lainnya{enter}")
-//     //Kategori Kegiatan
-//     cy.get('#select2-idrubrikkegiatan-container').click()
-//     cy.get('.select2-search__field').type("duduk{enter}")
-//     // SK Kegiatan
-//     cy.get('#iddokumen_label').click()
-//     cy.get('.odd > .text-center > .btn > .fa').click()
-
-//     cy.get('#tglterhitungmulai').type("30-08-2023")
-//     cy.get('#lokasipenugasan').type("Muara Enim")
-
-//     cy.get('.btn-success').contains("Simpan").click()
-
-//     cy.get('.modal-footer > .btn-primary').click()
-
-// })
+})
 
 Then("User berhasil menambahkan data", () => {
     cy.get('.alert').contains("Penambahan data jabatan tugas berhasil")
