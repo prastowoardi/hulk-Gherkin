887f0775e76afe7d8fd0db8142f0d2afec700937
prastowoardi <prastowoardi616@gmail.com>
2023-07-12 09:49:10 +0700

    [update] login

--- cypress/support/General/login.js
+++ cypress/support/General/login.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add('login', (user) => {
-    cy.contains('Masuk dengan akun Anda').should('be.visible')
+    cy.contains('Masuk dan Verifikasi').should('be.visible')
     cy.fixture('../fixtures/General/user').then((userdata) => {
-        cy.get('#userid').type(userdata[user].username)
+        cy.get('#email').type(userdata[user].username)
         cy.get('#password').type(userdata[user].password)
     })
-    cy.get('.btn').click()
+    cy.get('.btn-login').click()
 })
