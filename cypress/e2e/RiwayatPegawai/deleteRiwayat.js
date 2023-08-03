fe8404083bb4d09ee9208e0d39a06461a38a3375
prastowoardi <prastowoardi616@gmail.com>
2023-08-03 16:36:49 +0700

    fix delete data riwayat berdasarkan status

--- cypress/e2e/RiwayatPegawai/deleteRiwayat.js
+++ cypress/e2e/RiwayatPegawai/deleteRiwayat.js
@@ -1,15 +1,21 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
 When("Sudah ada data riwayat {string} yang dihapus", (menu) => {
-    cy.get(".table").find(".btn").first().click()
-    cy.get(".btn").contains("Edit").click()
-    cy.get('#select2-status-container').type('Draft{enter}')
-
-    cy.get('.btn-success').contains("Simpan").click()
-    cy.get('.btn-primary').click()
-
-    cy.get('.btn-danger > span').should('include.text','Hapus').click()
-    cy.get('.modal-footer > .btn-primary').click()
+    cy.get(".table").find(".label").then(($label) => {
+        if ($label.text().includes('Draft')) {
+            cy.get(".table").find(".btn").first().click()
+            cy.get('.btn-danger > span').should('include.text', 'Hapus').click()
+            cy.get('.modal-footer > .btn-primary').click()
+        } else {
+            cy.get(".table").find(".btn").first().click()
+            cy.get(".btn").contains("Edit").click()
+            cy.get('#select2-status-container').type('Draft{enter}')
+            cy.get('.btn-success').contains("Simpan").click()
+            cy.get('.btn-primary').click()
+            cy.get('.btn-danger > span').should('include.text', 'Hapus').click()
+            cy.get('.modal-footer > .btn-primary').click()
+        }
+    })    
 
     // Menampilkan jumlah data akan dihapus
     cy.get('.callout > .row > .col-md-3').contains('Data Akan Dihapus').next()
