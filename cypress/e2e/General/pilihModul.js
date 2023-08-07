919cd8341499f3f294b8d07ebc96c759ebfcc5e3
prastowoardi <prastowoardi616@gmail.com>
2023-08-07 09:10:15 +0700

    [update] fix pilih modul

--- cypress/e2e/General/pilihModul.js
+++ cypress/e2e/General/pilihModul.js
@@ -1,46 +1,46 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
-const moduleUserActions = {
+const moduleActions = {
   // Tambah modul lain dan kombinasi user disini
-    "SDM_Admin": () => {
+  SDM: {
+    Admin: () => {
       cy.get('.hr > .inner').click()
-      cy.get('#hr').contains("Administrator Perguruan Tinggi")
-        .should('be.visible', { timeout: 1000 }).click()
+      cy.get('#hr').contains("Administrator Perguruan Tinggi").should('be.visible', { timeout: 1000 }).click()
     },
-    "SDM_Dosen": () => {
+    Dosen: () => {
       cy.get('.hr > .inner').click()
-      cy.get('#hr').contains("Dosen")
-        .should('be.visible', { timeout: 1000 }).click()
+      cy.get('#hr').contains("Dosen").should('be.visible', { timeout: 1000 }).click()
+    }
+  },
+  PMB: {
+    Admin: () => {
+      cy.get(".spmb").click()
+      cy.get('#spmb').contains('Administrator Perguruan Tinggi').should('be.visible', { timeout: 1000 }).click()
+    }
+  },
+  Siakad: {
+    Admin: () => {
+      cy.get(".siakad > .inner").click()
+      cy.get("#siakad").contains("Administrator Perguruan Tinggi").should('be.visible', { timeout: 1000 }).click()
     },
-    "PMB_Admin": () => {
-      cy.get(".spmb").click(); //pilih modul pmb
-      cy.get('#spmb').contains('Administrator Perguruan Tinggi')
-        .should('be.visible', { timeout: 1000 }).click()
-    },
-    "Siakad_Admin": () => {
-      cy.get(".siakad > .inner").click();
-      cy.get("#siakad").contains("Administrator Perguruan Tinggi")
-      .should('be.visible', { timeout: 1000 }).click()
-    },
-    "Siakad_Dosen": () => {
-      cy.get(".siakad > .inner").click();
-      cy.get("#siakad").contains("Dosen")
-      .should('be.visible', { timeout: 1000 }).click()
-    },
-    "Keuangan_Admin": () => {
-      cy.get(".keuangan > .inner").click();
-      cy.get("#keuangan").contains("Administrator Perguruan Tinggi")
-      .should('be.visible', { timeout: 1000 }).click()
+    Dosen: () => {
+      cy.get(".siakad > .inner").click()
+      cy.get("#siakad").contains("Dosen").should('be.visible', { timeout: 1000 }).click()
+    }
+  },
+  Keuangan: {
+    Admin: () => {
+      cy.get(".keuangan > .inner").click()
+      cy.get("#keuangan").contains("Administrator Perguruan Tinggi").should('be.visible', { timeout: 1000 }).click()
     }
   }
-  
-  When('{string} masuk ke modul {string}', (user, modul) => {
-    const action = moduleUserActions[`${modul}_${user}`]
-    if (action) {
-      action()
-    } else {
-      // Handle the case when the combination of modul and user doesn't match any entry in the moduleUserActions object
-      cy.log("Modul / Role tidak ditemukan")
-    }
-  })
-  
+}
+
+When('{string} masuk ke modul {string}', (user, modul) => {
+  const action = moduleActions[modul]?.[user]
+  if (action) {
+    action()
+  } else {
+    cy.log("Modul / Role tidak ditemukan")
+  }
+})
