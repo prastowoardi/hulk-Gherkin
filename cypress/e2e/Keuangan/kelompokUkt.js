f8e60406efa0fc0dc9713bb89fc25c705f821b95
Ardi <prastowoardi616@gmail.com>
2023-08-15 10:12:50 +0700

    [update] tarif ukt dengan cicilan 3

--- cypress/e2e/Keuangan/kelompokUkt.js
+++ cypress/e2e/Keuangan/kelompokUkt.js
@@ -15,10 +15,16 @@ When("Admin klik tombol {string}", (buttonName) => {
 })
 
 When("Admin melihat alert {string}", (message) => {
-    if (message == "Berhasil"){
-        cy.get('.alert').contains("Penambahan data Kelompok UKT berhasil")
-    } else if (message == "Hapus"){
-        cy.get('.alert').contains("Penghapusan data Kelompok UKT berhasil")
+    const successMessages = {
+        "Berhasil": "Penambahan data Kelompok UKT berhasil",
+        "Hapus": "Penghapusan data Kelompok UKT berhasil",
+        "tarif ukt berhasil": "Penambahan data Tarif UKT berhasil"
+    }
+
+    const successMessage = successMessages[message]
+
+    if (successMessage) {
+        cy.get('.alert').contains(successMessage)
     } else {
         cy.log("Penambahan data Gagal !")
     }
@@ -37,53 +43,45 @@ When("Admin menghapus data {string}", (uktName) => {
               // Lakukan tindakan pada dua elemen pertama, dapat menyesuaikan kondisi sesuai kebutuhan
               cy.wrap($value).click()
             }
-          });
+          })
     })
-    // cy.log("Data ada di list kelompok UKT !")
 })
 
-When("Admin mengisi field {string} dengan {string}", (fieldName,fieldValue) => {
-    if (fieldName == "Kode"){
-        cy.get('#i_idkelompokukt').type(fieldValue)
-    } else if (fieldName == "Nama"){
-        cy.get('#i_namakelompokukt').type(fieldValue)
-    } else if (fieldName == "KIP Kuliah"){
-        if (fieldValue == "1"){
-            cy.get('.iCheck-helper').click()
-        } else {
-            cy.get('.iCheck-helper')
-        }
-    } else if (fieldName == 'periode masuk'){
-        cy.get('#idperiode').type(fieldValue,+'{enter}')
-    } else if (fieldName == 'gelombang'){
-        cy.get('#idgelombang').select(fieldValue)
-    } else if (fieldName == 'sistem kuliah'){
-        cy.get('#idsistemkuliah').select(fieldValue)
-    } else if (fieldName == 'jalur pendaftaran'){
-        cy.get('#idjalurpendaftaran').select(fieldValue)
-    } else if (fieldName == 'kelompok ukt'){
-        cy.get('#idkelompokukt').select(fieldValue)
-    } else if (fieldName == 'nominal tarif'){
-        cy.get('#nominaltarif').type(fieldValue)
-    } else if (fieldName == 'kuota'){
-        cy.get('#kuotaukt').type(fieldValue)
-    } else if (fieldName == 'jumlah cicilan'){
-        cy.get('#jmlcicilan').select(fieldValue)
-    } else if (fieldName == 'cicilan 1'){
-        cy.get('#cicilan_1').type(fieldValue)
-    } else if (fieldName == 'cicilan 2'){
-        cy.get('#cicilan_2').type(fieldValue)
-    } else if (fieldName == 'cicilan 3'){
-        cy.get('#cicilan_3').type(fieldValue)
-    } else if (fieldName == ''){
+const fieldSelectors = {
+    "Kode": "#i_idkelompokukt",
+    "Nama": "#i_namakelompokukt",
+    "KIP Kuliah": ".iCheck-helper",
+    "periode masuk": "#idperiode",
+    "gelombang": "#idgelombang",
+    "sistem kuliah": "#idsistemkuliah",
+    "jalur pendaftaran": "#idjalurpendaftaran",
+    "prodi": "#select2-idunit-container",
+    "kelompok ukt": "#idkelompokukt",
+    "nominal tarif": "#nominaltarif",
+    "kuota": "#kuotaukt",
+    "jumlah cicilan": "#jmlcicilan",
+    "cicilan 1": "#cicilan_1",
+    "cicilan 2": "#cicilan_2",
+    "cicilan 3": "#cicilan_3",
+    "frekuensi denda": "#idfrekuensi",
+    "nominal denda": "#nominaldenda",
+    "max denda": "#maxdenda"
+}
 
-    } else if (fieldName == ''){
+When("Admin mengisi field {string} dengan {string}", (fieldName, fieldValue) => {
+    const selector = fieldSelectors[fieldName]
 
-    } else if (fieldName == ''){
+    if (!selector) {
+        return // No matching selector found, do nothing
+    }
 
-    } else if (fieldName == ''){
-
-    } else if (fieldName == ''){
-
+    if (fieldName === "KIP Kuliah") {
+        fieldValue === "1" ? cy.get(selector).click() : cy.get(selector)
+    } else if (["periode masuk", "prodi"].includes(fieldName)) {
+        cy.get(selector).type(fieldValue + '{enter}')
+    } else if (["gelombang", "sistem kuliah", "jalur pendaftaran", "kelompok ukt", "jumlah cicilan", "frekuensi denda"].includes(fieldName)) {
+        cy.get(selector).select(fieldValue)
+    } else {
+        cy.get(selector).type(fieldValue)
     }
 })
