fe60a6d27c0fcef26b59a2b74d0a56f6d0c2c71c
prastowoardi <prastowoardi616@gmail.com>
2023-08-29 20:44:24 +0700

    [update] case edit data dan tambah data duplikat

--- cypress/Case/PMB/RPL/masterDokumenRPL.feature
+++ cypress/Case/PMB/RPL/masterDokumenRPL.feature
@@ -10,12 +10,13 @@ Feature: Admin Membuat Master Data
         * Admin isi "kode dokumen" dengan "<kode>"
         * Admin isi "nama dokumen" dengan "<nama>"
         * Admin "simpan" data
+        * Admin melihat informasi "berhasil tambah"
 
 Examples:
     | kode| nama      |
-    | A1  | Ijazah    |
-    | A2  | Transkrip |
-    | A3  | Silabus   |
+    | A4  | Sertifikat    |
+    | A5  | Pas Foto |
+    | A6  | Scan KK  |
 
     Scenario: Negatif - Admin menambahkan data master dokumen RPL tanpa kode
         When Admin klik tombol "Tambah"
@@ -41,3 +42,17 @@ Examples:
         * Admin isi "kode dokumen" dengan "ABCDE123456"
         * Admin isi "nama dokumen" dengan "Ijazah"
         * Admin "simpan" data
+
+    Scenario: Positif - Admin mengubah data master dokumen RPL
+        When Admin mencari data "Sertifikat" lalu klik edit
+        * Admin ubah isi "kode dokumen" dengan "A3 Edited"
+        * Admin ubah isi "nama dokumen" dengan "Sertifikat Edited"
+        * Admin "simpan edit" data
+        * Admin melihat informasi "berhasil edit"
+
+    Scenario: Positif - Admin menambah data master dokumen RPL dengan data yang sama
+        When Admin klik tombol "Tambah"
+        * Admin ubah isi "kode dokumen" dengan "A3"
+        * Admin ubah isi "nama dokumen" dengan "Sertifikat"
+        * Admin "simpan" data
+        * Admin melihat informasi "duplikat"
--- cypress/e2e/PMB/dokumenRPL.js
+++ cypress/e2e/PMB/dokumenRPL.js
@@ -5,16 +5,39 @@ const input = {
     "nama dokumen": "#i_namadokumenrpl"
 }
 
-const button= {
-    "simpan": ":nth-child(3) > .btn-success > .fa"
+When ("Admin isi {string} dengan {string}", (fieldName,fieldValue) => {
+        cy.get(input[fieldName]).type(fieldValue)
+})
+
+const edit = {
+    "kode dokumen": "#u_kodedokumenrpl",
+    "nama dokumen": "#u_namadokumenrpl"
 }
 
-When ("Admin isi {string} dengan {string}", (fieldName,fieldValue) => {
-    if (input[fieldName]) {
-        cy.get(input[fieldName]).type(fieldValue)
-    } 
+When ("Admin ubah isi {string} dengan {string}", (fieldName,fieldValue) => {
+    cy.get(edit[fieldName]).clear().type(fieldValue)
 })
+
+const button= {
+    "simpan": ":nth-child(3) > .btn-success > .fa",
+    "simpan edit": ":nth-child(3) > .btn-success[data-id='8']"
+}
 
 When ("Admin {string} data", (action) => {
     cy.get(button[action]).click()
+})
+
+const alert = {
+    "berhasil tambah": ".alert:contains('Penambahan data Dokumen RPL berhasil')",
+    "berhasil edit": ".alert:contains('Pengubahan data Dokumen RPL berhasil')",
+    "hapus berhasil": ".alert:contains('Penghapusan data Dokumen RPL berhasil')",
+    "duplikat": ".alert:contains('')"
+}
+
+When ("Admin melihat informasi {string}", (alertMessage) => {
+    cy.get(alert[alertMessage])
+})
+
+When ("Admin mencari data {string} lalu klik edit", (dataName) => {
+    cy.get('.table').contains(dataName).parent().find('.btn-warning').click()
 })
