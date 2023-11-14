d85534dda0fdba0b28b193687ee81b179a6dd2a9
prastowoardi <prastowoardi616@gmail.com>
2023-11-14 13:27:45 +0700

    credentials update

--- cypress/e2e/General/login.js
+++ cypress/e2e/General/login.js
@@ -5,9 +5,13 @@ Given("{string} masuk ke siakad", (role) =>{
     cy.visit("gate/login")
     if (role == "Admin"){
         cy.login(0)
+    } else if (role == "Super Administrator"){
+        cy.login(1)
     } else if (role == "Dosen"){
         cy.login(2)
     } else if (role == "Asesor Akuntansi"){
         cy.login(4)
+    } else if (role == "Admin Support Sevima"){
+        cy.login(5)
     }
 })
