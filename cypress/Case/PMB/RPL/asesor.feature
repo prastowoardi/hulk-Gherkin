9711e8a5d67a6376d3b33140a5ea2c7e5080e61f
prastowoardi <prastowoardi616@gmail.com>
2023-11-16 16:33:49 +0700

    tambah case nilai asesmen

--- cypress/Case/PMB/RPL/asesor.feature
+++ cypress/Case/PMB/RPL/asesor.feature
@@ -4,10 +4,16 @@ Feature: User Login Sebagai Asesor
         Given "Asesor TI" masuk ke siakad
         * "Asesor" masuk ke modul "PMB"
 
-    Scenario: Asesor mengakses halaman asesor
+    Scenario: Asesor memberi asesmen pada matakuliah yang dipilih pendaftar
         When "Asesor" menuju ke halaman "spmb/seleksi_rpl"
         * User melihat statistik pendaftar
         * Asesor berada pada halaman seleksi RPL
         * Asesor melakukan penilaian RPL pendaftar "DENI"
+        * Asesor "menyimpan" nilai pendaftar
+        * Asesor kembali ke halaman list dan melihat status pendaftar "DENI"
+
+    Scenario: Aseser menambahkan matakuliah yang tidak dipilih pendaftar
+        When "Asesor" menuju ke halaman "spmb/seleksi_rpl"
+        * Asesor menambahkan "2" matakuliah yang tidak dipilih pendaftar "DENI"
         * Asesor "menyimpan" nilai pendaftar
         * Asesor kembali ke halaman list dan melihat status pendaftar "DENI"
--- cypress/e2e/RPL/asesor.js
+++ cypress/e2e/RPL/asesor.js
@@ -85,3 +85,63 @@ When ("Asesor kembali ke halaman list dan melihat status pendaftar {string}", (n
   })
     
 })
+
+When ('Asesor menambahkan {string} matakuliah yang tidak dipilih pendaftar {string}', (jumlahMK,namaPendaftar) => {
+  cy.get("#sub-peminat-penilaian_rpl").click()
+    cy.get('#sub-peminat-penilaian_rpl > h4 > .badge').invoke('text').then((text) => {
+        cy.log("Pendaftar yang perlu dinilai: " + text)
+    })
+
+    cy.get('#search-input').type(namaPendaftar + '{enter}')
+
+    // Masuk ke detail pendaftar untuk memberikan nilai
+    cy.get('.col-sm-12').contains(namaPendaftar).parent().parent().next().next().next().next().children().click()
+    
+    // Memberikan asessmen RPL
+    cy.get('#form_list > div > div > div > div.card-header > div:nth-child(5) > div.col-md-7 > div > div > table:nth-child(4)').find('tr').then(($rows) => {
+        const numRows = $rows.slice(1).length
+        cy.log(`Jumlah mata kuliah yang harus dinilai: ${numRows}`)
+
+        // Daftar nilai yang diinginkan
+        const nilai = ['A', 'B', 'C', 'D', 'E']
+
+        $rows.each((index, $row) => {
+          if (jumlahMK < 5) {
+            // Menghasilkan indeks acak antara 0 hingga 4 (indeks array)
+            const indeksAcak = Math.floor(Math.random() * nilai.length)
+
+            // Menggunakan indeks acak untuk memilih nilai acak dari array
+            const nilaiAcak = nilai[indeksAcak]
+
+            // Di dalam setiap baris, cari elemen select dengan atribut 'name' yang sesuai
+            const nameAttribute = Cypress.$($row).find('.form-select.form-select-rpl.nangka').prop('name')
+            if (nameAttribute) {
+              // Menggunakan ekspresi reguler untuk mengambil angka dari atribut name
+              const matches = nameAttribute.match(/nangka_(\d+)/)
+              if (matches && matches.length > 1) {
+                const angka = matches[1] // Mengambil angka yang cocok
+                // Memilih nilai pada select yang sesuai
+                cy.get(`select[name^="nangka_${angka}"]`).select(nilaiAcak)
+                jumlahMK ++
+              }
+            } else {
+              // cy.log('element: ' +nameAttribute)
+              cy.log('Atribut name tidak ditemukan.')
+            }
+          } else {
+            // Apabila sudah memberikan nilai pada 3 data, hentikan proses selanjutnya
+            return false
+          }
+        })
+
+          // Melihat mata kuliah yang sudah di nilai
+          cy.get('#info-jumlah').invoke('text').then((text) => {
+            const arrayText = text.split(' ')
+            if (text.length > 5 ) {
+              cy.log(text)
+            } else {
+              cy.log("Mata kuliah yang sudah dinilai: " + arrayText[7])
+            }
+          })
+    })
+})
