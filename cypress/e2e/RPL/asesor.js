b76b9915cfe9d986f5fc0942059779c24f2edbb8
prastowoardi <prastowoardi616@gmail.com>
2023-11-22 21:51:20 +0700

    skip penilaian mk pendaftar pada  baris ke 7

--- cypress/e2e/RPL/asesor.js
+++ cypress/e2e/RPL/asesor.js
@@ -13,54 +13,59 @@ When ("User melihat statistik pendaftar", () => {
 When ("Asesor melakukan penilaian RPL pendaftar {string}", (namaPendaftar) => {
     cy.get("#sub-peminat-penilaian_rpl").click()
     cy.get('#sub-peminat-penilaian_rpl > h4 > .badge').invoke('text').then((text) => {
-        cy.log("Pendaftar yang perlu dinilai: " + text)
+      cy.log("Pendaftar yang perlu dinilai: " + text)
     })
 
     cy.get('#search-input').type(namaPendaftar + '{enter}')
 
     // Masuk ke detail pendaftar untuk memberikan nilai
     cy.get('.col-sm-12').contains(namaPendaftar).parent().parent().next().next().next().next().children().click()
-    
+
     // Memberikan asessmen RPL
     cy.get('#form_list > div > div > div > div.card-header > div:nth-child(5) > div.col-md-7 > div > div > table:nth-child(2)').find('tr').then(($rows) => {
-        const numRows = $rows.slice(1).length
-        cy.log(`Jumlah mata kuliah yang harus dinilai: ${numRows}`)
+      const numRows = $rows.slice(1).length
+      cy.log(`Jumlah mata kuliah yang harus dinilai: ${numRows}`)
 
-        // Daftar nilai yang diinginkan
-        const nilai = ['A', 'B', 'C', 'D', 'E']
+      // Daftar nilai yang diinginkan
+      const nilai = ['A', 'B', 'C', 'D', 'E']
 
-        $rows.each((index, $row) => {
-            // Menghasilkan indeks acak antara 0 hingga 4 (indeks array)
-            const indeksAcak = Math.floor(Math.random() * nilai.length)
+      $rows.each((index, $row) => {
+        // Mengabaikan baris ke-3 dan kelipatannya
+        if ((index + 1) % 7 === 0) {
+          cy.log(`Melewati baris ke-${index + 1}`)
+          return // Melanjutkan ke baris selanjutnya
+        }
 
-            // Menggunakan indeks acak untuk memilih nilai acak dari array
-            const nilaiAcak = nilai[indeksAcak]
+        // Menghasilkan indeks acak antara 0 hingga 4 (indeks array)
+        const indeksAcak = Math.floor(Math.random() * nilai.length)
+        // Menggunakan indeks acak untuk memilih nilai acak dari array
+        const nilaiAcak = nilai[indeksAcak]
 
-            // Di dalam setiap baris, cari elemen select dengan atribut 'name' yang sesuai
-            const nameAttribute = Cypress.$($row).find('.form-select.form-select-rpl.nangka').prop('name')
-            if (nameAttribute) {
-              // Menggunakan ekspresi reguler untuk mengambil angka dari atribut name
-              const matches = nameAttribute.match(/nangka_(\d+)/)
-              if (matches && matches.length > 1) {
-                const angka = matches[1] // Mengambil angka yang cocok
-                // Memilih nilai pada select yang sesuai
-                cy.get(`select[name^="nangka_${angka}"]`).select(nilaiAcak)
-              }
-            } else {
-              // cy.log('element: ' +nameAttribute)
-              cy.log('Atribut name tidak ditemukan.')
-            }
-          })
+        // Di dalam setiap baris, cari elemen select dengan atribut 'name' yang sesuai
+        const nameAttribute = Cypress.$($row).find('.form-select.form-select-rpl.nangka').prop('name')
+        if (nameAttribute) {
+          // Menggunakan ekspresi reguler untuk mengambil angka dari atribut name
+          const matches = nameAttribute.match(/nangka_(\d+)/)
+          if (matches && matches.length > 1) {
+            const angka = matches[1] // Mendapatkan angka yang cocok
+            // Memilih nilai pada select yang sesuai
+            cy.get(`select[name^="nangka_${angka}"]`).select(nilaiAcak)
+          }
+        } else {
+          // cy.log('element: ' +nameAttribute)
+          cy.log('Atribut name tidak ditemukan.')
+        }
+      })
 
-          // Melihat mata kuliah yang sudah di nilai
-          cy.get('#info-jumlah').invoke('text').then((text) => {
-            const arrayText = text.split(' ')
-            if (text.length > 5 ) {
-              cy.log(text)
-            } else {
-              cy.log("Mata kuliah yang sudah dinilai: " + arrayText[7])
-            }
-          })
+      // Melihat mata kuliah yang sudah di nilai
+      cy.get('#info-jumlah').invoke('text').then((text) => {
+        const arrayText = text.split(' ')
+        if (text.length > 5) {
+          cy.log(text)
+        } else {
+          cy.log("Mata kuliah yang sudah dinilai: " + arrayText[7])
+        }
+      })
     })
 })
 
